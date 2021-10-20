5864cbc8f5536fd05c73658b32293904aec4fb4f
GrandMoff100 <minecraftcrusher100@gmail.com>
2021-10-20 17:16:00 +0000

    Added flake8 and gitignore

--- virtualfiles/utils.py
+++ virtualfiles/utils.py
@@ -13,10 +13,13 @@ def load_bin_content(directory: Union[str, Path]) -> bytes:
     for file in find_files(Path(directory)):
         with open(file, 'rb') as f:
             data[str(file)] = zlib.compress(f.read())
-    
+
     return pickle.dump(data, f)
 
 def export_dir_to_bin(directory: Union[str, Path], target: Union[str, Path, None] = None) -> None:
+    if target is None:
+        target = "directory.data"
+
     with open(target, "wb") as f:
         content = load_bin_content(directory)
         f.write(content)
